5b7129b08c6f5683d99d86297554825bf4d5aba9
Elio <elionoula@gmail.com>
2021-05-07 00:44:07 -0400

    Not sure what, how, or why, but it works

--- db/init.sql
+++ db/init.sql
@@ -3,11 +3,12 @@ USE `pythonlogin`;
 
 CREATE TABLE IF NOT EXISTS `accounts` (
 	`id` int(11) NOT NULL AUTO_INCREMENT,
+    `fullname` varchar(200) NOT NULL,
   	`username` varchar(50) NOT NULL,
   	`password` varchar(255) NOT NULL,
   	`email` varchar(100) NOT NULL,
     PRIMARY KEY (`id`)
 ) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8;
 
-INSERT INTO `accounts` (`id`, `username`, `password`, `email`) VALUES (1, 'test', 'test', 'test@test.com');
+INSERT INTO `accounts` (`id`, `fullname`, `username`, `password`, `email`) VALUES (1,'test', 'test', 'test', 'test@test.com');
 
--- pythonglogin/app.py
+++ pythonglogin/app.py
@@ -17,56 +17,62 @@ app.config['MYSQL_DATABASE_PORT'] = 3306
 app.config['MYSQL_DATABASE_DB'] = 'pythonlogin'
 mysql.init_app(app)
 
-@app.route('/', methods=['GET', 'POST'])
+
+@app.route('/pythonlogin/', methods=['GET', 'POST'])
 def login():
+    # connect
     conn = mysql.connect()
     cursor = conn.cursor(pymysql.cursors.DictCursor)
+
+    # Output message if something goes wrong...
     msg = ''
+    # Check if "username" and "password" POST requests exist (user submitted form)
     if request.method == 'POST' and 'username' in request.form and 'password' in request.form:
+        # Create variables for easy access
         username = request.form['username']
         password = request.form['password']
-        cursor = mysql.get_db().cursor()
+        # Check if account exists using MySQL
         cursor.execute('SELECT * FROM accounts WHERE username = %s AND password = %s', (username, password))
+        # Fetch one record and return result
         account = cursor.fetchone()
+
+        # If account exists in accounts table in out database
         if account:
+            # Create session data, we can access this data in other routes
             session['loggedin'] = True
             session['id'] = account['id']
             session['username'] = account['username']
+            # Redirect to home page
+            # return 'Logged in successfully!'
             return redirect(url_for('home'))
         else:
+            # Account doesnt exist or username/password incorrect
             msg = 'Incorrect username/password!'
-    return render_template('index.html', msg='')
 
-@app.route('/logout')
-def logout():
-   session.pop('loggedin', None)
-   session.pop('id', None)
-   session.pop('username', None)
-   return redirect(url_for('login'))
-
-@app.route('/profile')
-def profile():
-    conn = mysql.connect()
-    cursor = conn.cursor(pymysql.cursors.DictCursor)
-    if 'loggedin' in session:
-        cursor.execute('SELECT * FROM accounts WHERE id = %s', (session['id'],))
-        account = cursor.fetchone()
-        return render_template('profile.html', account=account)
-    return redirect(url_for('login'))
+    return render_template('index.html', msg=msg)
 
 
+# http://localhost:5000/register - this will be the registration page
 @app.route('/register', methods=['GET', 'POST'])
 def register():
+    # connect
     conn = mysql.connect()
     cursor = conn.cursor(pymysql.cursors.DictCursor)
+
+    # Output message if something goes wrong...
     msg = ''
     # Check if "username", "password" and "email" POST requests exist (user submitted form)
     if request.method == 'POST' and 'username' in request.form and 'password' in request.form and 'email' in request.form:
+        # Create variables for easy access
+        fullname = request.form['fullname']
         username = request.form['username']
         password = request.form['password']
         email = request.form['email']
+
+        # Check if account exists using MySQL
         cursor.execute('SELECT * FROM accounts WHERE username = %s', (username))
         account = cursor.fetchone()
+        # If account exists show error and validation checks
         if account:
             msg = 'Account already exists!'
         elif not re.match(r'[^@]+@[^@]+\.[^@]+', email):
@@ -76,17 +82,55 @@ def register():
         elif not username or not password or not email:
             msg = 'Please fill out the form!'
         else:
-            cursor.execute('INSERT INTO accounts VALUES (NULL, %s, %s, %s)', (username, password, email))
+            # Account doesnt exists and the form data is valid, now insert new account into accounts table
+            cursor.execute('INSERT INTO accounts VALUES (NULL, %s, %s, %s, %s)', (fullname, username, password, email))
             conn.commit()
+
             msg = 'You have successfully registered!'
     elif request.method == 'POST':
+        # Form is empty... (no POST data)
         msg = 'Please fill out the form!'
+    # Show registration form with message (if any)
     return render_template('register.html', msg=msg)
 
-@app.route('/home')
+
+# http://localhost:5000/home - this will be the home page, only accessible for loggedin users
+@app.route('/')
 def home():
+    # Check if user is loggedin
     if 'loggedin' in session:
+        # User is loggedin show them the home page
         return render_template('home.html', username=session['username'])
+    # User is not loggedin redirect to login page
+    return redirect(url_for('login'))
+
+
+# http://localhost:5000/logout - this will be the logout page
+@app.route('/logout')
+def logout():
+    # Remove session data, this will log the user out
+    session.pop('loggedin', None)
+    session.pop('id', None)
+    session.pop('username', None)
+    # Redirect to login page
+    return redirect(url_for('login'))
+
+
+# http://localhost:5000/profile - this will be the profile page, only accessible for loggedin users
+@app.route('/profile')
+def profile():
+    # Check if account exists using MySQL
+    conn = mysql.connect()
+    cursor = conn.cursor(pymysql.cursors.DictCursor)
+
+    # Check if user is loggedin
+    if 'loggedin' in session:
+        # We need all the account info for the user so we can display it on the profile page
+        cursor.execute('SELECT * FROM accounts WHERE id = %s', [session['id']])
+        account = cursor.fetchone()
+        # Show the profile page with account info
+        return render_template('profile.html', account=account)
+    # User is not loggedin redirect to login page
     return redirect(url_for('login'))
 
 
