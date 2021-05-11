9253230cc9121ab3108d90b368812b8899b2f23c
elionoula <elionoula@gmail.com>
2021-05-11 14:15:57 -0400

    Reverted Changed For calendar.html

--- pythonglogin/app.py
+++ pythonglogin/app.py
@@ -1,13 +1,6 @@
-from __future__ import print_function
 from flask import Flask, render_template, request, redirect, url_for, session
 from flaskext.mysql import MySQL
 import pymysql
-import datetime
-import pickle
-import os.path
-import googleapiclient.discovery
-from google_auth_oauthlib.flow import InstalledAppFlow
-from google.auth.transport.requests import Request
 import re
 
 app = Flask(__name__)
@@ -143,43 +136,8 @@ def profile():
 @app.route('/calendar')
 def calendar():
     if 'loggedin' in session:
-        creds = None
-        SCOPES = ['https://www.googleapis.com/auth/calendar.readonly']
-        # The file token.pickle stores the user's access and refresh tokens, and is
-        # created automatically when the authorization flow completes for the first
-        # time.
-        if os.path.exists('token.pickle'):
-            with  open('token.pickle', 'rb') as token:
-                creds = pickle.load(token)
-        # If there are no (valid) credentials available, let the user log in.
-        if not creds or not creds.valid:
-            if creds and creds.expired and creds.refresh_token:
-                creds.refresh(Request())
-            else:
-                flow = InstalledAppFlow.from_client_secrets_file(
-                    'credentials.json', SCOPES)
-                creds = flow.run_local_server(port=0)
-                # Save the credentials for the next run
-                # with open('token.pickle', 'wb') as token: # can't write files in Google App Engine so comment out or delete
-                # pickle.dump(creds, token)
-        service = googleapiclient.discovery.build('calendar', 'v3', credentials=creds)
-        # Call the Calendar API
-        now = datetime.datetime.utcnow().isoformat() + 'Z'  # 'Z' indicates UTC time
-        print('Getting the upcoming 10 events')
-        events_result = service.events().list(calendarId='find your cal id from google and paste it here', timeMin=now,
-                                              maxResults=10, singleEvents=True, orderBy='startTime').execute()
-        events = events_result.get('items', [])
-        if not events:
-            print('No upcoming events found.')
-        # for event in events:
-        # start = event['start'].get('dateTime', event['start'].get('date'))
-        # print(start, event['summary'])
-        event_list = [event["summary"] for event in events]
-
-        return render_template("calendar.html", events=event_list)
+        return render_template("calendar.html", username=session['username'])
     return redirect(url_for('login'))
-
-
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', debug=True)
