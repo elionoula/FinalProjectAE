47c52d71703157bb69a2093020682943c4bcfcd0
elionoula <elionoula@gmail.com>
2021-05-11 16:01:48 -0400

    Added all components for NEW calendar. (Year view a little off)

--- db/init.sql
+++ db/init.sql
@@ -12,3 +12,18 @@ CREATE TABLE IF NOT EXISTS `accounts` (
 
 INSERT INTO `accounts` (`id`, `fullname`, `username`, `password`, `email`) VALUES (1,'test', 'test', 'test', 'test@test.com');
 
+CREATE TABLE `event` (
+  `id` int unsigned NOT NULL AUTO_INCREMENT,
+  `title` varchar(255) NOT NULL,
+  `url` varchar(255) NOT NULL,
+  `class` varchar(255) NOT NULL,
+  `start_date` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+  `end_date` timestamp NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=1 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+insert  into `event`(`id`,`title`,`url`,`class`,`start_date`,`end_date`) values
+(1,'Example','http://www.example.com','event-sucess','2019-09-10 20:00:00','2019-09-10 20:01:02'),
+(2,'Roy Tutorials','https://roytuts.com','event-important','2019-09-15 19:00:00','2019-09-15 19:42:45'),
+(3,'Roy Tutorial','https://roytuts.com','event-info','2019-09-23 20:03:05','2019-09-24 08:45:53'),
+(4,'Roy Tutorial','https://roytuts.com','event-error','2019-09-24 20:03:05','2019-09-25 08:45:53');
